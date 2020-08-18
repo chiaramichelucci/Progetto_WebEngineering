67961fcb42e03ac7c1898fe8476aa826b493934b
Chiara M <48277192+chiaramichelucci@users.noreply.github.com>
2020-08-18 11:42:19 +0200

    dao

--- ProgettoWebEng/src/data/dao/SondaggioDAO.java
+++ ProgettoWebEng/src/data/dao/SondaggioDAO.java
@@ -1,5 +1,16 @@
 package data.dao;
 
 public interface SondaggioDAO {
+    Sondaggio createSondaggio();
+
+    Sondaggio getSondaggio(int sondaggio_key) throws DataException;
+
+    List<Sondaggio> getSondaggio(Issue issue) throws DataException;
+
+    List<Sondaggio> getSondaggio() throws DataException;
+
+    List<Sondaggio> getUnassignedSondaggio() throws DataException;
+
+    void storeSondaggio(Sondaggio sondaggio) throws DataException;
 
 }
