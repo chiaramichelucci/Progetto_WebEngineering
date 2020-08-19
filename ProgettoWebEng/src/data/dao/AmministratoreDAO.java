18541e3bfcc598740fc05a2703e27cecd08c892a
Chiara M <48277192+chiaramichelucci@users.noreply.github.com>
2020-08-19 09:56:19 +0200

    raffinamento DAO

--- ProgettoWebEng/src/data/dao/AmministratoreDAO.java
+++ ProgettoWebEng/src/data/dao/AmministratoreDAO.java
@@ -1,16 +1,17 @@
 package data.dao;
 
+import java.util.List;
+import data.DataException;
+import data.model.Amministratore;
+
 public interface AmministratoreDAO {
+	
     Amministratore createAmministratore();
-
     Amministratore getAmministratore(int amministratore_key) throws DataException;
-
-    List<Amministratore> getAmministratore(Issue issue) throws DataException;
-
+    List<Amministratore> getAmministratore(Amministratore amministratore) throws DataException;
     List<Amministratore> getAmministratore() throws DataException;
-
-    List<Amministratore> getUnassignedAmministratore() throws DataException;
-
     void storeAmministratore(Amministratore amministratore) throws DataException;
-
+    void save(Amministratore amministratore);	
+    void update(Amministratore amministratore); 	
+    void delete(Amministratore amministratore);
 }
--- ProgettoWebEng/src/data/dao/SondaggioDAO.java
+++ ProgettoWebEng/src/data/dao/SondaggioDAO.java
@@ -1,21 +1,18 @@
 package data.dao;
 
+import java.util.List;
 import data.DataException;
-import data.impl.SondaggioImpl;
+import data.model.Sondaggio;
 
 public interface SondaggioDAO {
+	
     Sondaggio createSondaggio();
-
     Sondaggio getSondaggio(int sondaggio_key) throws DataException;
-
-    List<Sondaggio> getSondaggio(Issue issue) throws DataException;
-
+    List<Sondaggio> getSondaggio(Sondaggio sondaggio) throws DataException;
     List<Sondaggio> getSondaggio() throws DataException;
-
-    List<Sondaggio> getUnassignedSondaggio() throws DataException;
-
     void storeSondaggio(Sondaggio sondaggio) throws DataException;
-
-	SondaggioImpl getSondaggio(int sondaggio_id) throws DataException;
+    void save(Sondaggio sondaggio);	
+    void update(Sondaggio sondaggio); 	
+    void delete(Sondaggio sondaggio);
 
 }
