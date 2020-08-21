5f15b57d2bd460109aa04c78d707def9d91f8548
StratulatDragos <58547406+StratulatDragos@users.noreply.github.com>
2020-08-21 15:30:42 +0200

    Servlet

--- ProgettoWebEng/src/controller/CreazioneSondaggio.java
+++ ProgettoWebEng/src/controller/CreazioneSondaggio.java
@@ -4,13 +4,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//classe per provare i valori pressi dalla pagina crea.html
-//guarda la console per vedere i valori ricevuti
+public class CreazioneSondaggio extends HttpServlet {
 
-public class Valori extends HttpServlet {
-
-public void service(HttpServletRequest req, HttpServletResponse res) {
-		
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+	
 		String[] domande = req.getParameterValues("domanda");
 		String[] tipi = req.getParameterValues("tipo");
 		String[] note = req.getParameterValues("nota");
@@ -21,11 +18,9 @@ public void service(HttpServletRequest req, HttpServletResponse res) {
 			System.out.print("Nota " + i + ":" + note[i]);
 		}
 		
-		
 		//System.out.print("Campo 1: " + line[0]);
 		//System.out.print("Campo 2: " + line[1]);
 		//System.out.print("Campo 3: " + line[2]);
-		
 	}
 	
 }
