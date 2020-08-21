29eefa7e6bf2e02d42874ad87a9c40f4c6fa10f0
Chiara M <48277192+chiaramichelucci@users.noreply.github.com>
2020-08-21 16:38:57 +0200

    correzione DAO.

--- ProgettoWebEng/src/data/dao/AmministratoreDAO.java
+++ ProgettoWebEng/src/data/dao/AmministratoreDAO.java
@@ -7,11 +7,15 @@ import data.model.Amministratore;
 public interface AmministratoreDAO {
 	
     Amministratore createAmministratore();
-    Amministratore getAmministratore(int amministratore_key) throws DataException;
+    Amministratore getAmministratore(int id_amministratore) throws DataException;
     List<Amministratore> getAmministratore(Amministratore amministratore) throws DataException;
     List<Amministratore> getAmministratore() throws DataException;
     void storeAmministratore(Amministratore amministratore) throws DataException;
     void save(Amministratore amministratore);	
     void update(Amministratore amministratore); 	
     void delete(Amministratore amministratore);
+	static boolean validate(String n, String p) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
